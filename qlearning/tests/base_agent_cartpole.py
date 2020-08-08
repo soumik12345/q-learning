87a0aca8496436a71571306b5355dd7687b1f1ca
soumik12345 <19soumik.rakshit96@gmail.com>
2020-08-08 23:46:19 +0530

    Updated Tests

--- qlearning/tests/base_agent_cartpole.py
+++ qlearning/tests/base_agent_cartpole.py
@@ -5,5 +5,5 @@ from qlearning.agents import BaseAgent
 def run_test():
     agent = BaseAgent(env_name='CartPole-v0')
     frames = agent.act(iterations=200, render=True)
-    wandb.log({"video": wandb.Video(frames, fps=4, format="mp4")})
+    wandb.log({"video": wandb.Video(frames, fps=10, format="mp4")})
     agent.close_env()
