f825bd51d4fcdf6200bb32908af3a7c138981069
soumik12345 <19soumik.rakshit96@gmail.com>
2020-08-09 02:27:02 +0530

    Added Frozen Lake BaseAgent Test

---
+++ qlearning/tests/base_agent_frozen_lake.py
@@ -0,0 +1,20 @@
+import wandb
+from gym.envs.registration import register
+from qlearning.agents.base import BaseAgent
+
+
+def run_test():
+    try:
+        register(
+            id='FrozenLakeNoSlip-v0',
+            entry_point='gym.envs.toy_text:FrozenLakeEnv',
+            kwargs={'map_name': '4x4', 'is_slippery': False},
+            max_episode_steps=100,
+            reward_threshold=0.78,
+        )
+    except Exception as e:
+        pass
+    agent = BaseAgent(env_name='FrozenLakeNoSlip-v0')
+    print(agent.is_env_discrete)
+    agent.act(iterations=200, render=True, collect_frames=False)
+    agent.close_env()
